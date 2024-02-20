f6049916cc2ea7ecacec2b6dcb34aff87aed6eb7
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-02-20 15:25:12 -0500

    reverting login.js

--- public/login.js
+++ public/login.js
@@ -2,7 +2,7 @@ const loginButton = document.getElementById('login');
 
 if (loginButton) {
     loginButton.addEventListener('click', function() {
-        window.location.href = 'https://practice-spotify-app.onrender.com/login';
+        window.location.href = 'http://localhost:5502/login';
     });
 
 }
