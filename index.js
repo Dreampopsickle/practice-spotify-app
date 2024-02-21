9ae1be63c458d28acbe0229049943cbc1d6e9429
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-02-20 20:40:27 -0500

    console log

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.use(express.static(path.join(__dirname, 'src')))
     .use(cookieParser());
 
 app.get('/', (req, res) => {
+    console.log(__dirname);
     res.sendFile(('login.html'));
 });
 
