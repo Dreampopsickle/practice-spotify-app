43ce45edf720d61ff4ce09fcafb9715c67cff9dd
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-03-03 18:13:28 -0500

    move refresh token mechanism to servers directory

--- server.js
+++ server.js
@@ -135,58 +135,12 @@ const { logOutRoute } = require('./routes/logOutRoute');
 logOutRoute(app);
 
 
-
-
-
-
-//Setup the access token
-
-
-async function refreshAccessToken() {
-    console.log('Attempting to refresh access token with refresh token:', refreshToken);
-
-    if (!refreshToken) {
-        console.error('No refresh token available.');
-        return;
-    }
-    
-    const refreshData = queryString.stringify({
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken
-    });
-
-    const refreshOptions = {
-        headers: {
-            'Authorization': 'Basic ' + Buffer.from(clientId + ":" + clientSecret).toString('base64'),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    };
-
-    axios.post(spotifyTokenUrl, refreshData, refreshOptions)
-        .then(response => {
-            if (response.data.access_token) {
-                accessToken = response.data.access_token;
-                console.log('Access token refreshed successfully');
-            }
-            if (response.data.refresh_token) {
-                refreshToken = response.data.refresh_token;
-            }
-        }).catch(error =>{
-            console.error('Error refreshing access token:', error);
-        })
-};
-
-async function refreshTokenIfNeeded() {
-    if (Date.now() > accessTokenExpiry) {
-        await refreshAccessToken();
-    }
-}
-
-
-// setAccessToken(accessToken, accessTokenExpiry);
+//Setup Refresh Token Mechanism
+const { refreshTokenIfNeeded } = require('./services/spotifyService');
 setInterval(refreshTokenIfNeeded, 60000);
 
-// const CACHE_TTL = 30000;
+
+//Fetch and Broadcast Spotify Data
 
 
 
---
+++ services/spotifyService.js
@@ -0,0 +1,41 @@
+const refreshAccessToken= async () => {
+    console.log('Attempting to refresh access token with refresh token:', refreshToken);
+
+    if (!refreshToken) {
+        console.error('No refresh token available.');
+        return;
+    }
+    
+    const refreshData = queryString.stringify({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+    });
+
+    const refreshOptions = {
+        headers: {
+            'Authorization': 'Basic ' + Buffer.from(clientId + ":" + clientSecret).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    axios.post(spotifyTokenUrl, refreshData, refreshOptions)
+        .then(response => {
+            if (response.data.access_token) {
+                accessToken = response.data.access_token;
+                console.log('Access token refreshed successfully');
+            }
+            if (response.data.refresh_token) {
+                refreshToken = response.data.refresh_token;
+            }
+        }).catch(error =>{
+            console.error('Error refreshing access token:', error);
+        })
+};
+
+const refreshTokenIfNeeded = async () => {
+    if (Date.now() > accessTokenExpiry) {
+        await refreshAccessToken();
+    }
+};
+
+module.exports = { refreshAccessToken, refreshTokenIfNeeded };
