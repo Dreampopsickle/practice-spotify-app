196265b9b39f5632469829d72bb7e54bedbae7b9
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-03-03 19:38:21 -0500

    fix path

--- routes/authStateRoute.js
+++ routes/authStateRoute.js
@@ -1,7 +1,8 @@
+const { path } = require('../config/index')
 const authStateRoute = (app) => {
     app.get('/authenticated', (req, res) => {
         // res.sendFile('/authenticated.html');
-        res.sendFile(path.join(__dirname, 'src', 'authenticated.html'));
+        res.sendFile(path.join(__dirname, '..', 'src', 'authenticated.html'));
     })
 };
 
