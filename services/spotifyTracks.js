7872997f1ad2188eef37b132351713e89b4680d7
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-03-09 11:44:46 -0500

    add await keyword

--- services/spotifyTracks.js
+++ services/spotifyTracks.js
@@ -102,15 +102,14 @@ const broadcastToClients = (trackInfo, ws) => {
 
 const fetchAndBroadcastCurrentPlaying = async (dependencies, options) => {
   // console.log("Options passed in: ", options);
-  // const { ws } = options;                                    ////////////////////////////////////////////////////////////
-  // console.log('Options in fetch function: ', options);       ////FIX the FLOW of OPTIONS to the rest of this function/////
-  ////////////////////////////////////////////////////////////
+  // const { ws } = options;
   if (retryAfter > Date.now()) {
     console.log("Rate limit in effect. Skipping fetch");
     scheduleNextFetch(dependencies, options);
     return;
   }
   const socket = options;
+  const trackData = await fetchAndBroadcastCurrentPlaying(dependencies);
   // console.log("What is in socket?: ", socket);
   // getCurrentTrackFromSpotify(callback, dependencies);
   handletrackData(trackData, socket);
