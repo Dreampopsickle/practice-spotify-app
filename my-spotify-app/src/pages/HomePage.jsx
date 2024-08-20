cfa4bb20011d6352e94941de844606e02b1c38c4
Christian Rakotoarisoa <CRakotoarisoa@gmail.com>
2024-08-20 14:43:17 -0400

    setup routes

---
+++ my-spotify-app/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import weLogo from "../assets/WElogo.png";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <img src={weLogo} alt="Logo" className="size-36" />
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/authenticated">Authenticated Page</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default HomePage;
