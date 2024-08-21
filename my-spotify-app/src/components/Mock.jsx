3084d082a477889893d9160be40be885c2811342
Christian Rakotoarisoa <CRakotoarisoa@gmail.com>
2024-08-21 17:11:34 -0400

    mock album

--- my-spotify-app/src/components/Mock.jsx
+++ my-spotify-app/src/components/Mock.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Mock = () => {
-  return <p>Test</p>;
+  return (
+    <div className="track-info">
+      <img
+        src="../assets/album-art.jpeg"
+        alt="Album Art"
+        className="album-art"
+      />
+      <div className="track-details">
+        <h2 className="track-title">Track: Serenade of Water</h2>
+        <p className="track-artist">Artist: Men I Trust</p>
+        <p className="track-album">Album: Untourable Album</p>
+      </div>
+    </div>
+  );
 };
 
 export default Mock;
